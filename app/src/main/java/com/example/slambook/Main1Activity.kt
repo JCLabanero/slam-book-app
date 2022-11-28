20e7e73e858b84951b31bc63785b9334398a4e28
John Carlo <johncarlolabanero@gmail.com>
2022-11-28 16:21:24 +0800

    adjust v28 add new activity(signup)

---
+++ app/src/main/java/com/example/slambook/Main1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.slambook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
+    }
+}
